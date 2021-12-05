2d5e181c093aef33fca3be1741f9854a7bb27979
GreenDuckStudio <69764730+GreenDuckStudio@users.noreply.github.com>
2021-12-05 14:05:34 +0100

    Initial commit

---
+++ src/main/java/de/greenduck/tutorialmod/Tutorialmod.java
@@ -0,0 +1,10 @@
+package de.greenduck.tutorialmod;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Tutorialmod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/de/greenduck/tutorialmod/client/TutorialmodClient.java
@@ -0,0 +1,13 @@
+package de.greenduck.tutorialmod.client;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+
+@Environment(EnvType.CLIENT)
+public class TutorialmodClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
